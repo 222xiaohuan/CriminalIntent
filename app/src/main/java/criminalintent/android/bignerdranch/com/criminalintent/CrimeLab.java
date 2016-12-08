d3ca0c99ec6fb8faf69c9d76197fb972bcb219c3
emily <435477171@qq.com>
2016-12-08 08:59:56 +0800

    CrimeLab类添加报错crimes对象到json方法

--- app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeLab.java
+++ app/src/main/java/criminalintent/android/bignerdranch/com/criminalintent/CrimeLab.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 import android.content.Context;
+import android.util.Log;
 
 public class CrimeLab {
     private ArrayList<Crime> mCrimes;
+
+    private static final String TAG = "CrimeLab";
+    private static final String FILENAME = "crimes.json";
+    private CriminalIntentJSONSerializer mSerializer;
 
     private static CrimeLab sCrimeLab;
     private Context mAppContext;
@@ -14,6 +19,7 @@ public class CrimeLab {
     private CrimeLab(Context appContext) {
         mAppContext = appContext;
         mCrimes = new ArrayList<Crime>();
+        mSerializer = new CriminalIntentJSONSerializer(mAppContext, FILENAME);
     }
 
     public static CrimeLab get(Context c) {
@@ -41,6 +47,17 @@ public class CrimeLab {
 
     public void deleteCrime(Crime c) {
         mCrimes.remove(c);
+    }
+
+    public boolean saveCrimes() {
+        try {
+            mSerializer.saveCrimes(mCrimes);
+            Log.d(TAG, "crimes saved to file");
+            return true;
+        }catch (Exception e) {
+            Log.e(TAG, "Error saving crimes:", e);
+            return false;
+        }
     }
 }
 
